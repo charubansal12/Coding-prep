d8f60f46aa473e7813da43ba386e4f4350a00003
frank_anthony <charubansal102@gmail.com>
2017-01-09 09:48:12 +0530

    Create MorrisTraversal.cpp
    References - http://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion-and-without-stack/

---
+++ MorrisTraversal.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<stdio.h>
+#include<stack>
+using namespace std;
+struct node
+{
+    int data;
+    node *left,*right;
+};
+node *newNode(int data)
+{
+    node *temp=new node;
+    temp->data=data;
+    temp->left=NULL;
+    temp->right=NULL;
+    return temp;
+}
+void inorder(node *root)
+{
+    node *curr=root;
+    while(curr!=NULL)
+    {
+        if(curr->left==NULL)
+        {
+            cout<<curr->data<<" ";
+            curr=curr->right;
+        }
+        else
+        {
+            node *pred=curr->left;
+            while(pred->right!=curr && pred->right!=NULL)
+                pred=pred->right;
+            if(pred->right==NULL)
+            {
+                pred->right=curr;
+                curr=curr->left;
+            }
+            else
+            {
+                pred->right=NULL;
+                cout<<curr->data<<" ";
+                curr=curr->right;
+            }
+        }
+    }
+}
+int main()
+{
+    node *root=newNode(1);
+    root->left        = newNode(2);
+  root->right       = newNode(3);
+  root->left->left  = newNode(4);
+  root->left->right = newNode(5);
+  inorder(root);
+  return 0;
+}
